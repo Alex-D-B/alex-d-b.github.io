1eb24bb8a9ad9c11c2c7ade66e30d8d45810e035
Alex Broihier <alexanderbroihier@gmail.com>
2023-08-01 20:14:32 -0500

    Removed unused imports / Counter element

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import svelteLogo from './assets/svelte.svg';
-    import viteLogo from '/vite.svg';
-    import Counter from './lib/Counter.svelte';
     import Sim from './lib/Sim.svelte';
     import ProjectCard from './lib/ProjectCard.svelte';
 </script>
