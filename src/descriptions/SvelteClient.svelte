27b28becacb951602f4860874216e2d57efe2b6c
Alex Broihier <alexanderbroihier@gmail.com>
2023-08-19 08:50:13 -0500

    Fixed issue where Svelte logo did not show up in built project

--- src/descriptions/SvelteClient.svelte
+++ src/descriptions/SvelteClient.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+    import svelteLogo from '../assets/svelte.svg';
+</script>
+
 <p>The very thing this is.</p>
-<img src="./src/assets/svelte.svg" alt="svelte logo" class="ml-auto mr-auto" />
+<img src={svelteLogo} alt="svelte logo" class="ml-auto mr-auto" />
 <ul class="list-disc list-inside">
     <li>Created a physics engine to power a unique background display for this website</li>
     <li>Compiled Rust code to web assembly to enable the physics engine to remain performant</li>
